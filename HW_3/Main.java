7f6e5e5e6998bef02aa9e4ad263de86c0f118624
Baranov Mikhail Sergeevich <baranov.mihail@mail.ru>
2023-07-04 20:49:25 +0300

    add HW_3

--- HW_3/Main.java
+++ HW_3/Main.java
@@ -3,7 +3,7 @@ package HW_3;
 public class Main {
     public static void main(String[] args) {
 
-        DataRequest dr = new DataRequest();
+        Data dr = new Data();
         dr.SaveDataEnter();
 
     }
